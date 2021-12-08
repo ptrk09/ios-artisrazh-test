1d9d0a2bbd88adfd1416824845d3fed6242d5da1
ptrk09 <paw.toporkov@gmail.com>
2021-12-08 15:19:31 +0300

    init progect

---
+++ List-Cats/List-Cats/Application/List_CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_CatsApp.swift
+//  List-Cats
+//
+//  Created by Павел Топорков on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct List_CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ List-Cats/List-Cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  List-Cats
+//
+//  Created by Павел Топорков on 08.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, cats!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
